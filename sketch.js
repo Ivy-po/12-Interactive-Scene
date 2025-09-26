ffc4163322cb7875ff6a48d67fc6d8c7b332cff7
Ivy-po <popovychivanna11@gmail.com>
2025-09-26 16:49:32 +0000

    loop

--- sketch.js
+++ sketch.js
@@ -23,6 +23,8 @@ let end;
 let pX = 300;
 let pY = 760;
 
+
+
 function preload() {
   end = loadImage('img/end.png')
   pl = loadImage('img/player.png')
@@ -32,24 +34,38 @@ function setup() {
   //This function get run once at the start of the program
   createCanvas(600, 800);
   background(240);
+
+
   // ellipseMode(CORNER);
   ellipseMode(CENTER);
   rectMode(CENTER);
-
   //Set the number of frames per second
   frameRate(30);
 }
 
+
 function draw() {
   background(100);
   noStroke();
-
   fill(100, 207, 66); // makes spaces created below green
   rect(300, 50, 600, 100); // green spaces for chicken to chill
   rect(300, 290, 600, 100);
   rect(300, 530, 600, 100);
   rect(300, 760, 600, 100);
-  image(end, 0, 0, 600, 50, 0, 0, 600, 50)
+  
+  
+  
+  let x = 100;
+ while (x <= 500){
+  fill ('white')
+  rect (x, 175, 100, 10);
+  rect (x, 410, 100, 10);
+  rect (x, 650, 100, 10);
+  x += 200;
+ }
+  
+ 
+ image(end, 0, 0, 600, 50, 0, 0, 600, 50)
   drawCar(xPos1, yPos1, 140, 'red'); //draws a car 1
   drawCar(xPos2, yPos2, 140, 'yellow'); //draws a car  2
   drawCar(xPos3, yPos3, 140, 'blue'); //draws a car 3
@@ -59,14 +75,32 @@ function draw() {
   movePlayer();
   drawPlayer(pX, pY);
 
+
+
+  if (pX >= 600 || pX <= 0) {
+    pX = 300;
+    pY = 760;
+  }
+
+  if (pY >= 800 || pY <= 0) {
+    pX = 300;
+    pY = 760;
+  }
+
+
   textSize(15);
   text('level: ' + level, 10, 30)
 
-   textSize(15);
+  textSize(15);
   text('lives: ' + lives, 550, 30)
 
-  if (lives <= 0){
+  if (lives <= 0) {
     level -= 1;
+  }
+
+  if (level <= 0) {
+    background('white');
+    text('duck died...', 300, 300);
   }
 
   if (pY <= 50) {
@@ -81,19 +115,19 @@ function draw() {
     xPos3 += 8;
   }
 
-    if (level == 3) {
+  if (level == 3) {
     xPos1 += 11;
     xPos2 -= 10;
     xPos3 += 9;
   }
 
-    if (level == 4) {
+  if (level == 4) {
     xPos1 += 12;
     xPos2 -= 11;
     xPos3 += 10;
   }
 
-      if (level == 5) {
+  if (level == 5) {
     xPos1 += 13;
     xPos2 -= 12;
     xPos3 += 11;
@@ -117,7 +151,7 @@ function draw() {
     }
   }
 
-    if (abs(pX - xPos1) <= 90) {
+  if (abs(pX - xPos1) <= 90) {
     if (abs(pY - yPos1) <= 55) {
       background('red')
       pX = 300;
@@ -126,7 +160,7 @@ function draw() {
     }
   }
 
-    if (abs(pX - xPos4) <= 90) {
+  if (abs(pX - xPos4) <= 90) {
     if (abs(pY - yPos4) <= 55) {
       background('red')
       pX = 300;
@@ -147,6 +181,7 @@ function draw() {
     xPos3 = random(-60, -400);
   }
 
+}
 
   function drawPlayer(x, y) {
     image(pl, x, y, 40, 40, 0, 0, 40, 40)
@@ -180,4 +215,3 @@ function draw() {
     rect(x + len / 3, y - len / 3, len / 4, len / 8);
   }
 
-}
